273611590fb81d7e46c51ea2bd218e05e99c47fa
funghicat87 <funghicat87@gmail.com>
2023-02-25 20:14:23 +0800

    修改內容

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import ToDo from "./pages/ToDo";
-import Snake from "./pages/Snake";
-import Stopwatch from "./pages/Stopwatch";
+// import Snake from "./pages/Snake";
+// import Stopwatch from "./pages/Stopwatch";
 import TicTacToe from "./pages/TicTacToe";
 
 function App() {
@@ -13,21 +13,21 @@ function App() {
           <div className='SidebarBTN'>
             <Link to="/ToDo">ToDo</Link>
           </div>
-          <div className='SidebarBTN'>
+          {/* <div className='SidebarBTN'>
             <Link to="/Stopwatch">Stopwatch</Link>
-          </div>
+          </div> */}
           <div className='SidebarBTN'>
             <Link to="/TicTacToe">TicTacToe</Link>
           </div>
-          <div className='SidebarBTN'>
+          {/* <div className='SidebarBTN'>
             <Link to="/Snake">Snake</Link>
-          </div>
+          </div> */}
         </div>
 
         <div className='Main'>
           <Routes>
-            <Route path="/Snake" element={<Snake />}/>
-            <Route path="/Stopwatch" element={<Stopwatch />}/>
+            {/* <Route path="/Snake" element={<Snake />}/>
+            <Route path="/Stopwatch" element={<Stopwatch />}/> */}
             <Route path="/ToDo" element={<ToDo />}/>
             <Route path="/TicTacToe" element={<TicTacToe />}/>
           </Routes>
